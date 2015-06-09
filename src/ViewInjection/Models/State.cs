9cda4a3f182850509586dbed1549aa41fd7ad73e
Simon Timms <stimms@gmail.com>
2015-06-08 22:59:06 -0600

    Initial add

---
+++ src/ViewInjection/Models/State.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ViewInjection.Models
+{
+    public class State
+    {
+        public string Abbreviation { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/ViewInjection/Services/StateService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using ViewInjection.Models;
+
+namespace ViewInjection.Services
+{
+    public interface IStateService
+    {
+        IEnumerable<State> List();
+    }
+    public class StateService : IStateService
+    {
+        public IEnumerable<State> List() {
+            return new List<State>
+            {
+                new State { Abbreviation = "AK", Name = "Alaska" },
+                new State { Abbreviation  = "AL", Name = "Alabama" }
+            };
+        }
+    }
+}
